87d0f6dda9fbdb26bfcbc23866e0f15de6bafb92
jo4o0rn3ll4s <joao.adriano.ornelas@gmail.com>
2023-02-12 23:13:47 -0300

    up2

--- tstJal.py
+++ tstJal.py
@@ -4,18 +4,19 @@ import PySimpleGUI as sg
 def main():
     sg.theme('DarkAmber')
     layout_main = [[sg.Text('Janela Principal')],
-                   [sg.Button('Janela_A'), sg.Button('Janela_B')],
-                   '''[sg.Button('Janela_C'),sg.Button('Janela_D')]''']
+                   [sg.Button('Janela_A'), sg.Button('Janela_B')]]
 
     janelaMain = sg.Window('teste', layout_main)
 
     while True:
-        event = janelaMain.read()
+        event = janelaMain.read()[0]
         if event == sg.WIN_CLOSED:  # if user closes window or clicks cancel
             break
         if event == 'Janela_A':
+            #janelaMain.close()
             jalA()
         if event == 'Janela_B':
+            #janelaMain.close()
             jalB()
     janelaMain.close()
 
@@ -23,18 +24,19 @@ def main():
 def jalA():
     sg.theme('DarkAmber')
     layout_A = [[sg.Text('Janela A')],
-                [sg.Button('Janela_Main'), sg.Button('Janela_B')],
-                '''[sg.Button('Janela_C'),sg.Button('Janela_D')]''']
+                [sg.Button('Janela_Main'), sg.Button('Janela_B')]]
 
     janelaA = sg.Window('teste', layout_A)
 
     while True:
-        event = janelaA.read()
+        event = janelaA.read()[0]
         if event == sg.WIN_CLOSED:  # if user closes window or clicks cancel
             break
         if event == 'Janela_Main':
-            main()
+            janelaA.close()
+            #main()
         if event == 'Janela_B':
+            janelaA.close()
             jalB()
     janelaA.close()
 
@@ -42,18 +44,19 @@ def jalA():
 def jalB():
     sg.theme('DarkAmber')
     layout_B = [[sg.Text('Janela B')],
-                [sg.Button('Janela_Main'), sg.Button('Janela_A')],
-                '''[sg.Button('Janela_C'),sg.Button('Janela_D')]''']
+                [sg.Button('Janela_Main'), sg.Button('Janela_A')]]
 
     janelaB = sg.Window('teste', layout_B)
 
     while True:
-        event = janelaB.read()
+        event = janelaB.read()[0]
         if event == sg.WIN_CLOSED:  # if user closes window or clicks cancel
             break
         if event == 'Janela_Main':
-            main()
+            janelaB.close()
+            #main()
         if event == 'Janela_A':
+            janelaB.close()
             jalA()
     janelaB.close()
 
