ec9e8927b250efb14cd2cd8fe3b0260a26a0875d
jo4o0rn3ll4s <joao.adriano.ornelas@gmail.com>
2023-02-18 12:27:11 -0300

    trabalho json

---
+++ ex_form/teste.py
@@ -0,0 +1,11 @@
+import json
+
+#'''
+with open('ex_form/teste.json', 'r') as trans:
+    teste = json.load(trans)
+#'''
+'''
+carros_json = '{"marca":["Honda","Chevrolet","Fiat"],"modelo":["escort","chevete","147"]}'
+teste = json.loads(carros_json)
+'''
+print(teste)
